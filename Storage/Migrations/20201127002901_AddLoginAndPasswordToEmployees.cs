0d270adee315dda0a4d9a59dca7e63c49a52338e
Mark Nikerin <mrnikerin@gmail.com>
2020-11-27 06:02:58 +0400

    Add User Login and Password

---
+++ Storage/Migrations/20201127002901_AddLoginAndPasswordToEmployees.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Storage.Migrations
+{
+    public partial class AddLoginAndPasswordToEmployees : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Employees");
+        }
+    }
+}
--- Storage/Models/Employee/Employee.cs
+++ Storage/Models/Employee/Employee.cs
@@ -7,6 +7,8 @@
     public class Employee : IEntity
     { 
         public int Id { get; set; } 
+        public string Login { get; set; }
+        public string Password { get; set; }
         public string FirstName { get; set; } 
         public string MiddleName { get; set; } 
         public string LastName { get; set; }
